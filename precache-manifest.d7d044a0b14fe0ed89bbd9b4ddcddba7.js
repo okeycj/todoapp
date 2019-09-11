93e2d13da3983c822ffa87557e524c8b78bdcb55
Azubuike Chijindu Okechukwu <30808024+okeycj@users.noreply.github.com>
2019-09-11 22:16:14 +0100

    Updates

---
+++ precache-manifest.d7d044a0b14fe0ed89bbd9b4ddcddba7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cb6e6054a1bab342f1a538199444d6",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "836a148c2c4e6a58e62a",
+    "url": "/todoapp/static/css/main.8e1ffcaa.chunk.css"
+  },
+  {
+    "revision": "8a1933d1d8d8593c60b6",
+    "url": "/todoapp/static/js/2.2c4b7a58.chunk.js"
+  },
+  {
+    "revision": "836a148c2c4e6a58e62a",
+    "url": "/todoapp/static/js/main.ab948677.chunk.js"
+  },
+  {
+    "revision": "2c7050630c4d8bf90f33",
+    "url": "/todoapp/static/js/runtime~main.0f0e817c.js"
+  }
+]);
